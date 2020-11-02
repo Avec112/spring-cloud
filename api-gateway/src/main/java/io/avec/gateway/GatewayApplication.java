263424dab1d483a44ab4d8a2d593a690dba3cbde
Avec <avec112@gmail.com>
2020-11-02 21:34:56 +0100

    - Renamed application.properties to bootstrap.properties for cloud modules - Renamed module frontendclient to frontend-client - Renamed module gateway to api-gateway - Added Hystrix to api-gateway and failover implementations - Changed default property eureka.server.renewal-percent-threshold from 0.85 to 0.49

--- api-gateway/src/main/java/io/avec/gateway/GatewayApplication.java
+++ api-gateway/src/main/java/io/avec/gateway/GatewayApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 public class GatewayApplication {
@@ -17,9 +20,20 @@ public class GatewayApplication {
 	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
 		return builder.routes()
 				.route(p -> p.path("/green")
+						.filters(f -> f.hystrix(config -> config.setName("mycmd").setFallbackUri("forward:/failed")))
 						.uri("lb://green"))
 				.route(p -> p.path("/blue")
+						.filters(f -> f.hystrix(config -> config.setName("mycmd").setFallbackUri("forward:/failed")))
 						.uri("lb://blue"))
 				.build();
+	}
+
+	@Controller
+	static class FailedController {
+		@RequestMapping("/failed")
+		public @ResponseBody
+		String failed() {
+			return "Service is not available. Please try again later.";
+		}
 	}
 }
